53cc01ba102bb9195c8c95b31a6c51d2870fb273
KoharuDaisukii <bong1926@naver.com>
2023-04-30 21:41:36 +0900

    main screen 추가

--- StudytimeCheck.c
+++ StudytimeCheck.c
@@ -31,24 +31,50 @@ int main(int argc, char* argv[])
 		exit(1);
 	}
 	
-	DIR* UID_dirptr = login(argv[1]); // 로그인해서 UID 이름의 폴더를 엶
-	closedir(UID_dirptr);
+	char UID[11]; strcpy(UID, argv[1]); // 
+	DIR* UID_dirptr = login(UID); // 로그인해서 UID 이름의 폴더를 엶
+	
+	initscr();
+	noecho();
+	move(1, 1);
+	printw("Welcome! %s", UID);
+	move(3, 1);
+	printw("1. Studytime Measuring");
+	move(4, 1);
+	printw("2. Display Stats");
+	move(5, 1);
+	printw("3. Group");
+	move(6, 1);
+	printw("4. Settings");
+	move(7, 1);
+	printw("5. Exit");
+	char menu;
+	while(1)
+	{
+		menu = getch();
+		// if(menu == '1') menu1();
+		// if(menu == '2') menu2();
+		// if(menu == '3') menu3();
+		// if(menu == '4') menu4();
+		if(menu == '5') break;
+	}
+	endwin();
+	closedir(UID_dirptr); // 로그아웃
+	printf("%s님 bye bye\n", UID);
 	return 0;
 }
 
-DIR* login(char* wantID)
+DIR* login(char* UID)
 {
 	DIR *dir_ptr;
 	struct dirent *direntp;
-	char UID[11];
-	strcpy(UID, wantID);
 	
 	if(chdir(USER_INFO_DIR) == -1) // 유저 정보가 담긴 디렉토리로 이동
 	{
 		perror(USER_INFO_DIR);
 		exit(1);
 	}
-	
+	// 한글 ID 입력하면 컷하는 기능도 필요할 듯
 	if((dir_ptr = opendir(UID)) == NULL) // UID 폴더 존재하는 지 확인
 	{
 		printf("ID가 존재하지 않습니다. 해당 ID로 가입하시겠습니까? (Y/N)\n");
@@ -69,6 +95,7 @@ DIR* login(char* wantID)
 			if((dir_ptr = opendir(UID)) == NULL) // UID 디렉토리 열기
 			{
 				perror("안 열려요");
+				exit(1);
 			}
 			printf("가입 완료되었습니다. %s님 환영합니다.\n", UID);
 			sleep(3); // 로딩하는 척
@@ -77,6 +104,7 @@ DIR* login(char* wantID)
 		{
 			printf("\'가입하지 않기\'를 선택하셨습니다. 프로그램이 종료됩니다.\n");
 			sleep(3);
+			exit(1);
 		}
 	}
 	
