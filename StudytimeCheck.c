7b725e2db8896776482c756fddce7472a163511c
KoharuDaisukii <bong1926@naver.com>
2023-05-14 17:27:57 +0900

    users 폴더와 파일 없어도 동작하게 수정

--- StudytimeCheck.c
+++ StudytimeCheck.c
@@ -6,22 +6,23 @@
 #include <sys/types.h> // mkdir, lseek
 #include <fcntl.h> // open
 #include <dirent.h> // struct dirent
-#include <unistd.h> // chdir, write, lseek
+#include <unistd.h> // mkdir, chdir, write, lseek, dup(1,2)
 #include <curses.h>
 #include <time.h> // time, struct tm
 
-#define USER_INFO_DIR "users"
-#define USER_INFO_FILE "user_info.txt"
+#define USERS_INFO_DIR "users"
+#define USERS_INFO_FILE "users.txt"
 #define NO_GROUP "default"
 
 typedef struct Studyuser
 {
 	char user_ID[11];
 	char group_ID[11];
-	// 가입 일자
-	// 최종 접속 시간
+	time_t signup;// 가입 일자
+	time_t lastaccess;// 최종 접속 시간
 	// 또 뭐 넣지
 } Studyuser;
+
 
 typedef struct timelog // 공부 시간 기록을 저장하는 구조체
 {	
@@ -34,6 +35,7 @@ typedef struct timelog // 공부 시간 기록을 저장하는 구조체
 
 DIR* login(char* UID); // login 성공: UID directory 포인터 return
 
+int usersFd; // USERS_INFO_FILE file descriptor
 
 int main(int argc, char* argv[])
 {
@@ -45,12 +47,12 @@ int main(int argc, char* argv[])
 	else if(strlen(argv[1]) >= 11) // 아이디 10글자 이하로 입력 안 함
 	{
 		fprintf(stderr, "아이디는 10글자 이하로 입력해주세요.\n");
-		exit(1);
+		exit(2);
 	}
 	else if(argc >= 3) // 쓸데없는 것 입력함
 	{
 		fprintf(stderr, "ID만 입력해주세요. Usage: ./StudytimeCheck YOUR_ID\n");
-		exit(1);
+		exit(3);
 	}
 	
 	char UID[11]; strcpy(UID, argv[1]); // 
@@ -67,9 +69,11 @@ int main(int argc, char* argv[])
 	move(5, 1);
 	printw("3. Group");
 	move(6, 1);
-	printw("4. Settings");
+	printw("4. My profile");
 	move(7, 1);
-	printw("5. Exit");
+	printw("5. Settings");
+	move(8, 1);
+	printw("6. Exit");
 	char menu;
 	while(1)
 	{
@@ -78,10 +82,12 @@ int main(int argc, char* argv[])
 		// if(menu == '2') menu2();
 		// if(menu == '3') menu3();
 		// if(menu == '4') menu4();
-		if(menu == '5') break;
+		// if(menu == '5') menu5();
+		if(menu == '6') break;
 	}
 	endwin();
 	closedir(UID_dirptr); // 로그아웃
+	close(usersFd);
 	printf("%s님, Bye Bye!\n", UID);
 	return 0;
 }
@@ -89,29 +95,52 @@ int main(int argc, char* argv[])
 DIR* login(char* UID)
 {
 	DIR *dir_ptr;
-	
-	if(chdir(USER_INFO_DIR) == -1) // 유저 정보가 담긴 디렉토리로 이동, 없어도 알아서 만들어야 하는데 고치기 귀찮음
+
+	if(chdir(USERS_INFO_DIR) == -1) // 유저 정보가 담긴 디렉토리로 이동
 	{
-		perror(USER_INFO_DIR);
-		exit(1);
+		if(mkdir(USERS_INFO_DIR, 0755) == -1 || chdir(USERS_INFO_DIR) == -1) // 없으면 만듦
+		{
+			perror(USERS_INFO_DIR);
+			exit(4);
+		}
+
 	}
-	
-	int fd, user_exist = 0; //
-	if((fd = open(USER_INFO_FILE, O_RDWR)) == -1) // 유저들의 정보를 담아놓은 파일을 Open, 얘도 없어도 알아서 creat 해야 하는데
+
+	int tempfd1, tempfd2;
+	int user_exist = 0; //
+	if((tempfd1 = open(USERS_INFO_FILE, O_RDWR | O_CREAT | O_EXCL, 0777)) == -1) // 파일 만들기 시도
 	{
-		perror("왜 안 열려");
-		exit(1);
+		if((tempfd2 = open(USERS_INFO_FILE, O_RDWR)) == -1) // 이미 있네
+		{
+			perror("open fd2");
+			exit(9);
+		}
+		if((usersFd = dup(tempfd2)) == -1) // usersFd에 tempfd2 복제
+		{
+			perror("dup2-1");
+			exit(9);
+		}
+		close(tempfd2);
 	}
-	
+	else // 파일 없었을 때(이 시점에서는 존재함)
+	{
+		if((usersFd = dup(tempfd1)) == -1) // usersFd에 tempfd2 복제
+		{
+			perror("dup2-2");
+			exit(9);
+		}
+		close(tempfd1);
+	}
+
 	Studyuser s_userptr;
-	while(read(fd, &s_userptr, sizeof(Studyuser)) >= sizeof(Studyuser))
+	while(read(usersFd, &s_userptr, sizeof(Studyuser)) >= sizeof(Studyuser))	
 		if(strcmp(s_userptr.user_ID, UID) == 0) // 해당하는 UID의 유저가 있는지 탐색
 			user_exist = 1; // 있네
-	
+			
 	// 한글 ID 입력하면 컷하는 기능도 필요할 듯
 	if((dir_ptr = opendir(UID)) == NULL || user_exist == 0) // UID 폴더 존재하는 지 확인, 그룹 생각 안 하고 일단 함
 	{
-		printf("ID가 존재하지 않습니다. 해당 ID로 가입하시겠습니까? (Y/N)\n");
+		printf("ID가 존재하지 않습니다. 해당 ID로 가입하시겠습니까? (Y/N): ");
 		char yesno = toupper(getchar());
 		while(yesno != 'Y' && yesno != 'N') // y도 아니고 n도 아니면 반복
 		{
@@ -121,24 +150,23 @@ DIR* login(char* UID)
 
 		if(yesno == 'Y') // Y 입력
 		{
-			if(mkdir(UID, 0777) == -1) // UID 이름의 디렉토리 만듦 = 가입
+			if(mkdir(UID, 0755) == -1) // UID 이름의 디렉토리 만듦 = 가입
 			{
 				perror("mkdir error");
-				exit(1);
+				exit(6);
 			}
 			if((dir_ptr = opendir(UID)) == NULL) // UID 디렉토리 열기
 			{
 				perror("안 열려요");
-				exit(1);
+				exit(7);
 			}
 			
 			// user_info 파일에 유저 정보 추가
 			Studyuser newuser;
 			strcpy(newuser.user_ID, UID);
 			strcpy(newuser.group_ID, NO_GROUP);
-			lseek(fd, 0, SEEK_END);
-			write(fd, &newuser, sizeof(Studyuser));
-			close(fd);
+			lseek(usersFd, 0, SEEK_END);
+			write(usersFd, &newuser, sizeof(Studyuser));
 			// 추가 완료
 			
 			printf("가입 완료되었습니다. %s님 환영합니다.\n", UID);
@@ -148,7 +176,7 @@ DIR* login(char* UID)
 		{
 			printf("\'가입하지 않기\'를 선택하셨습니다. 프로그램이 종료됩니다.\n");
 			sleep(2);
-			exit(1);
+			exit(0);
 		}
 	}
 	
