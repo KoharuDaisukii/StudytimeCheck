dcbbfe87ea57da3d94c73a72f8dbea439e577d36
KoharuDaisukii <bong1926@naver.com>
2023-05-28 22:17:40 +0900

    wsctotime(초를 받아 dd hh mm ss 형식으로 윈도우에 출력해줌) 함수 추가

--- varlib.h
+++ varlib.h
@@ -6,10 +6,13 @@
 
 int arrow_convert(int key_select, int arrow_select, int bound);
 
+void wsctotime(WINDOW*, int seconds);
 void wcleardel(WINDOW*);
 void wdraw_timebar(WINDOW*, int y_start, int x_start);
 void mvwprintw_standout(WINDOW*, int y, int x, char*, int line_num, int arrow_select);
 void wfill(WINDOW*, int y1, int x1, int y2, int x2, char* ch);
 
 int rmdir_r(char* path);
+
+
 
